81112fdf67868d959d0706f8afbe18bdba9d445b
Nathan Waggoner <nwagg14@vt.edu>
2014-10-16 16:52:17 -0400

    Modified NewRegionToRegionRectangleConnect, created stub for test

---
+++ src/test/java/model/MARK_II/connectTypes/NewRegionToRegionRectangleConnectTest.java
@@ -0,0 +1,12 @@
+package model.MARK_II.connectTypes;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by nwagg14@vt.edu on 10/16/14.
+ */
+public class NewRegionToRegionRectangleConnectTest extends TestCase{
+    public void setUp(){
+        
+    }
+}
