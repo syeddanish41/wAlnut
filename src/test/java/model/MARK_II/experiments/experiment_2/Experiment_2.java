9fef9b914628079b713564ed038fe2d39f0fb565
Quinn Liu <quinnliu@vt.edu>
2015-07-01 13:45:51 -0400

    new class for showing how TP/TM work

---
+++ src/test/java/model/MARK_II/experiments/experiment_2/Experiment_2.java
@@ -0,0 +1,20 @@
+package model.MARK_II.experiments.experiment_2;
+
+import java.io.IOException;
+
+/**
+ * @author Q Liu (quinnliu@vt.edu)
+ * @date 7/1/2015.
+ */
+public class Experiment_2 {
+    private static String pathToExperiment_2_folder = "./src/test/java/model/experiments/vision/MARK_II" +
+            "/experiment_2/";
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Running Experiment_2.main() ...");
+
+
+        
+        System.out.println("Finished Experiment_2.main()");
+    }
+}
