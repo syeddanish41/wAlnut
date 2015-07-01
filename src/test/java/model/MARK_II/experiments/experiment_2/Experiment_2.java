d20c680fb4318eb4e906c3f31e643c331066fffb
Quinn Liu <quinnliu@vt.edu>
2015-07-01 13:57:18 -0400

    working on TP example

--- src/test/java/model/MARK_II/experiments/experiment_2/Experiment_2.java
+++ src/test/java/model/MARK_II/experiments/experiment_2/Experiment_2.java
@@ -1,5 +1,13 @@
 package model.MARK_II.experiments.experiment_2;
 
+import model.MARK_II.connectTypes.AbstractSensorCellsToRegionConnect;
+import model.MARK_II.connectTypes.SensorCellsToRegionRandomConnect;
+import model.MARK_II.connectTypes.SensorCellsToRegionRectangleConnect;
+import model.MARK_II.generalAlgorithm.SpatialPooler;
+import model.MARK_II.generalAlgorithm.TemporalPooler;
+import model.MARK_II.region.Region;
+import model.MARK_II.sensory.Retina;
+
 import java.io.IOException;
 
 /**
@@ -13,8 +21,29 @@ public class Experiment_2 {
     public static void main(String[] args) throws IOException {
         System.out.println("Running Experiment_2.main() ...");
 
+        // NOTE: the following parameters are borrowed from numenta/nupic TP example
+        // here: https://github.com/numenta/nupic/blob/master/examples/tp/hello_tp.py
 
-        
+        // Step 1: create Temporal Pooler instance with appropriate parameters
+        Retina retina = new Retina(10, 10);
+        Region region = new Region("root", 6, 8, 2, 10, 3);
+        region.setInhibitionRadius(3);
+
+        AbstractSensorCellsToRegionConnect retinaToRegion = new SensorCellsToRegionRectangleConnect();
+        retinaToRegion.connect(retina.getVisionCells(), region.getColumns(),
+                0, 0);
+
+        SpatialPooler spatialPooler = new SpatialPooler(region);
+        spatialPooler.setLearningState(true);
+
+        TemporalPooler temporalPooler = new TemporalPooler(spatialPooler, 10);
+
+//        this.retina.seeBMPImage("2.bmp");
+//        this.spatialPooler.performPooling();
+//        this.temporalPooler.performPooling();
+//        this.temporalPooler.nextTimeStep();
+
+
         System.out.println("Finished Experiment_2.main()");
     }
 }
