bc1f169bb8d3460c5fc0852f5cef16dec48feb51
Q Liu <quinnliu@vt.edu>
2015-09-03 08:04:56 -0400

    created intial file

---
+++ src/main/java/model/MARK_II/generalAlgorithm/failedResearch/PredictionAlgorithm_1.java
@@ -0,0 +1,13 @@
+package model.MARK_II.generalAlgorithm.failedResearch;
+
+/**
+ * An experimental prediction algorithm. Initial ideas behind how this algorithm
+ * was designed are here: https://github.com/WalnutiQ/walnut/issues/199
+ *
+ * @author Q Liu (quinnliu@vt.edu)
+ * @version 9/3/2015
+ */
+public class PredictionAlgorithm_1 {
+    
+
+}
