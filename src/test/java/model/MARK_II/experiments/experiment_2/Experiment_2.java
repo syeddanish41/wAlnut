97687b3aebb36bc6fd474833ce5e6b189d7f1a9d
Q Liu <quinnliu@vt.edu>
2016-01-18 16:14:08 -0500

    finished implementing running one time step

--- src/test/java/model/MARK_II/experiments/experiment_2/Experiment_2.java
+++ src/test/java/model/MARK_II/experiments/experiment_2/Experiment_2.java
@@ -7,6 +7,9 @@ import model.MARK_II.connectTypes.AbstractRegionToRegionConnect;
 import model.MARK_II.connectTypes.AbstractSensorCellsToRegionConnect;
 import model.MARK_II.connectTypes.RegionToRegionRectangleConnect;
 import model.MARK_II.connectTypes.SensorCellsToRegionRectangleConnect;
+import model.MARK_II.generalAlgorithm.failureResearch.spatialAlgorithms.SDRAlgorithm_1;
+import model.MARK_II.generalAlgorithm.failureResearch.temporalAlgorithms.PredictionAlgorithm_1;
+import model.MARK_II.region.Layer5Region;
 import model.MARK_II.region.Region;
 import model.MARK_II.sensory.ImageViewer;
 import model.MARK_II.sensory.Retina;
@@ -14,52 +17,60 @@ import model.MARK_II.util.Point3D;
 import model.MARK_II.util.Rectangle;
 import java.awt.Point;
 
-import java.util.Arrays;
-
 /**
- * Created by qliu on 1/17/16.
- * Created by raarathi on 1/17/16.
+ * Refer to Experiment 2 drawing here: https://github.com/WalnutiQ/wAlnut/issues/199
+ *
+ * @author raarathi
+ * @author Q Liu (quinnliu@vt.edu)
+ * @version 1/17/16
  */
 public class Experiment_2 extends TestCase {
     private ImageViewer imageViewer;
     private Region regionA;
-    private Region regionB;
+    private Layer5Region regionB;
     private Region regionC;
+    private SDRAlgorithm_1 sdrAlgorithm_1;
+    private PredictionAlgorithm_1 predictionAlgorithm_1;
 
    public void setUp() throws IOException{
        Retina retina = new Retina(64, 64);
 
        regionA = new Region("A", 32, 32, 1, 20, 3);
-       regionB = new Region("B", 32, 32, 1, 20, 3);
+       regionB = new Layer5Region("B", 32, 32, 1, 20, 3);
        regionC = new Region("C", 16, 16, 1, 20, 3);
 
-       // Connecting retina to region B
        AbstractSensorCellsToRegionConnect retinaToRegion = new SensorCellsToRegionRectangleConnect();
-       retinaToRegion.connect(retina.getVisionCells(), this.regionB.getColumns(), 0, 0);
+       retinaToRegion.connect(retina.getVisionCells(), this.regionA.getColumns(), 0, 0);
 
        AbstractRegionToRegionConnect regionToRegion = new RegionToRegionRectangleConnect();
-       // Connecting region A to B
-       regionToRegion.connect(this.regionB.getColumns(), this.regionA.getColumns(), 0, 0);
-       // Connecting region A to C:left
+       regionToRegion.connect(this.regionA.getColumns(), this.regionB.getColumns(), 0, 0);
        regionToRegion.connect(this.regionA.getColumns(), this.regionC.getColumns(new Rectangle(new Point(0,0), new Point(7,15))), 0, 0);
-       // Connecting region B to C:right
        regionToRegion.connect(this.regionB.getColumns(), this.regionC.getColumns(new Rectangle(new Point(7,0), new Point(15,15))), 0, 0);
 
-       this.imageViewer = new ImageViewer("2_minus_1.bmp", retina);
+       this.imageViewer = new ImageViewer("2_minus_1.bmp", retina); // 400 x 400 pixels
+       this.imageViewer.saccadeRetinaToNewPositionAndGetWhatItSees(new Point3D(200, 200, 200));
+
+       this.sdrAlgorithm_1 = new SDRAlgorithm_1(1000, this.regionA, 10);
+       this.sdrAlgorithm_1.setLearningState(true);
+       this.predictionAlgorithm_1 = new PredictionAlgorithm_1(this.sdrAlgorithm_1);
+       this.predictionAlgorithm_1.setLearningState(true);
+   }
+
+    void runAlgorithmOneTimeStep() throws IOException {
+        this.sdrAlgorithm_1.run(); // on Region A
+        this.sdrAlgorithm_1.changeRegion(this.regionB);
+        this.sdrAlgorithm_1.run(); // on Region B
+        this.sdrAlgorithm_1.changeRegion(this.regionC);
+        this.sdrAlgorithm_1.run(); // on Region C
+
+        this.predictionAlgorithm_1.run(); // on Region C
+
+        Point3D nextRetinaPosition = this.regionB.getMotorOutput(
+                this.imageViewer.getBoxRetinaIsStuckIn());
+        this.imageViewer.saccadeRetinaToNewPositionAndGetWhatItSees(nextRetinaPosition);
     }
 
     public void test_experiment_2() {
         assertEquals(2-1, 1);
     }
-
-
-//    int[][] seenAreaFittedToRetinaSize = this.imageViewer.saccadeRetinaToNewPositionAndGetWhatItSees(new Point3D(33, 33, 33));
-//    assertTrue(Arrays.deepEquals(this.twoDotBMP, seenAreaFittedToRetinaSize));
-//    assertEquals(66, seenAreaFittedToRetinaSize.length);
-//    assertEquals(66, seenAreaFittedToRetinaSize[0].length);
-//
-//    int[][] shiftToTheLeftAndZoomIn = this.imageViewer.saccadeRetinaToNewPositionAndGetWhatItSees(new Point3D(16, 33, 25));
-//    assertEquals(66, shiftToTheLeftAndZoomIn.length);
-//    assertEquals(66, shiftToTheLeftAndZoomIn[0].length);
-//    assertTrue(Arrays.deepEquals(this.twoShiftedToTheLeftZoomedIn, shiftToTheLeftAndZoomIn));
 }
