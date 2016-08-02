0a9d10e72f0f69d8f2befaf3a54a3e179fa9324b
Q Liu <quinnliu@vt.edu>
2016-08-02 18:35:00 +0800

    added first dataset to try to read image by image

--- model/network.py
+++ model/network.py
@@ -2,7 +2,7 @@ class Network(object):
     """A set of layers arranged in a hierarchy from largest layer at the
     bottom and the smallest layer with a single node at the top.
 
-    They are connected bottom up. Example network is model/images/network_structure.png.
+    They are connected bottom up. Example network is model/images/explanatory/network_structure.png.
     """
     def __init__(self, layers):
         self.layers = layers
